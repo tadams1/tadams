5c983deba6f8402ec60f1226484f4192382d8740
Toby Adams <tadams1@gmail.com>
2015-11-24 07:22:40 +1100

    refactor to classs

---
+++ JavaScript/gl2/trianglegenerator.js
@@ -0,0 +1,114 @@
+var TriGenerator = function() {
+	var width;
+	var height;
+
+	TriGenerator.prototype.init = function(w,h) {
+		this.width = w;
+		this.height = h;
+	}
+
+	TriGenerator.prototype.getVerticesCount = function () {
+		 return (this.width+1) * (this.height+1)*3;
+	}
+
+	 TriGenerator.prototype.getIndicesCount = function() {
+      var numIndPerRow = this.width * 2 + 2;
+      var numIndDegensReq = (this.height - 1) * 2;
+      return numIndPerRow * this.height + numIndDegensReq;
+    	//return (this.width*2+2) * this.height + ((this.height-1)*2);
+  	}
+
+	TriGenerator.prototype.getVectices = function() {
+    var w = this.width+1;
+    var h = this.height+1;
+		var total_vertices = w * h *3;
+    	vert = new Float32Array(total_vertices);
+    	normals = new Float32Array(total_vertices);
+
+    	for(var y = 0; y < h; y++) {
+      		var base = y * w;
+      		for(var x = 0; x < w; x++) {
+        		var index = (base + x)*3;
+        		vert[index]=x;
+        		vert[index+1]=y;
+        		vert[index+2] = 0;
+            normals[index]=0;
+            normals[index+1]=0;
+            normals[index+2]=1;
+          }
+		  }
+
+   		return {
+    		vertices: vert,
+      	normals: normals
+    	}
+	}
+
+	TriGenerator.prototype.getIndices = function() {
+    var i = 0;
+ 		indices = new Uint16Array(this.getIndicesCount(this.width,this.height));
+    for(var y = 0; y < this.height; y++) { 
+      var base = y * (this.width+1);
+			for(var x = 0; x < this.width+1; x++) {
+        indices[i++] = ((base + x));
+        indices[i++] = ((base + this.width + x))+1;
+      }
+
+	    if(y < this.height-1) {
+        indices[i++] = ((y + 1) * (this.width+1) + (this.width));
+	      indices[i++] = ((y + 1) * (this.width+1));
+      }
+    }
+    return indices;
+  }
+}
+
+function SphereBumpGenerator() {
+  TriGenerator.call(this);
+}
+
+SphereBumpGenerator.prototype = Object.create(TriGenerator.prototype); 
+SphereBumpGenerator.prototype.constructor = SphereBumpGenerator;
+
+SphereBumpGenerator.prototype.init = function(w,h,r,x1,y1) {
+  TriGenerator.prototype.init(w,h);
+  this.radius = r;
+  this.x = x1;
+  this.y = y1;
+}
+
+SphereBumpGenerator.prototype.getVectices = function() {
+  var w = this.width+1;
+  var h = this.height+1;
+  console.log(w);
+  console.log(h);
+  var total_vertices = w * h *3;
+  vert = new Float32Array(total_vertices);
+  normals = new Float32Array(total_vertices);
+
+  for(var y = 0; y < h; y++) {
+    var base = y * w;
+    for(var x = 0; x < w; x++) {
+      var index = (base + x)*3;
+      var c = ((x-this.x)*(x-this.x))+((y-this.y)*(y-this.y));
+      vert[index]=x;
+      vert[index+1]=y;
+      console.log("c + "+ c + " " + this.r)
+      if(c<Math.pow(this.radius,2)) {
+        vert[index+2]=-1*(this.radius-Math.sqrt(c));
+        normals[index]=(x-this.x)/this.radius;
+        normals[index+1]=(y-this.y)/this.radius;
+        normals[index+2]=(this.radius-Math.sqrt(c))/this.radius;
+      } else {
+        vert[index+2] = 0;
+        normals[index] = 0;
+        normals[index+1] = 0;
+        normals[index+2]= 1;
+      }
+    }
+  }
+  return {
+    vertices: vert,
+    normals: normals  
+  } 
+}
