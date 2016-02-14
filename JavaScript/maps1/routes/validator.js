2feb66fb3464461a38eddf61997bb44a79c0d671
Toby Adams <tadams1@gmail.com>
2016-02-14 15:37:34 +1100

    latest

--- JavaScript/maps1/routes/validator.js
+++ JavaScript/maps1/routes/validator.js
@@ -1,19 +1,56 @@
 var express = require('express');
 var router = express.Router();
 var cfg = require('../config')
+var ObjectID = require('mongodb').ObjectID;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	var layers = req.app.get('layers');
 	var collection = layers.db.collection(cfg.mongodb.col);
 
-/*"properties.validated": false*/
-  collection.find().toArray(function(e,docs){ 
-
-	 
-    console.log(JSON.stringify(docs));
+  var clause = {"properties.validated": false};
+  collection.find(clause).toArray(function(e,docs){ 
     res.render('validator', {records: JSON.stringify(docs)});
   });
 });
+
+router.post('/', function(req, res, next) {
+  var layers = req.app.get('layers');
+  var collection = layers.db.collection(cfg.mongodb.col);
+  console.log(req.body.valid.length);
+
+  for(var i=0; i<req.body.valid.length; i++) {
+    console.log("Validated" + req.body.valid[i]);
+    collection.updateOne(
+    {
+      "_id": ObjectID.createFromHexString(req.body.valid[i])
+    },
+    {
+      $set: {
+        "properties.validated": true
+      }
+    }, function(err, results){
+      console.log(results);
+    });
+  }
+
+  for(var i=0; i < req.body.invalid.length;i++) {
+
+    console.log("Deleting" + req.body.valid[i]);
+    collection.deleteOne(
+    {
+      "_id": ObjectID.createFromHexString(req.body.invalid[i])
+    }, function(err, results){
+      console.log(results);
+    });
+  } 
+
+  console.log("DONE");
+  res.status(200);
+  res.send({"result": "success"});
+  res.end();
+});
+
 
 module.exports = router;
